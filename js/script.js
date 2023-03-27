96cd2cf0b483627482f36c9a366f852563d07c17
Onyinye-Reca <rebeccaobiajuru@gmail.com>
2023-03-27 19:42:23 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 //toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');   
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 //scroll sections active link
 let sections = document.querySelectorAll("section");
@@ -19,42 +19,72 @@ window.onscroll = () => {
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach(links => {
+      navLinks.forEach((links) => {
         links.classList.remove("active");
-        document.querySelector('header nav a[href*=' + id + ']').classList.add("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
       });
     }
   });
 
   //sticky navbar
-  let header = document.querySelector('header');
+  let header = document.querySelector("header");
 
-  header.classList.toggle('sticky', window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 100);
 
   //remove toggle icon and navbar when navbar link is clicked (scroll)
-  menuIcon.classList.remove('bx-x');
-  navbar.classList.remove('active');   
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
 //scroll reveal
-ScrollReveal({ 
+ScrollReveal({
   //reset: true,
-  distance: '800px',
+  distance: "800px",
   duration: 2000,
-  delay: 200
+  delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-image', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
-
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-image", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 //typed js
-const typed = new Typed('.multiple-text', {
-  strings: ['Software Engineer', 'Sociologist'],
+const typed = new Typed(".multiple-text", {
+  strings: ["Software Engineer", "Sociologist"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
-  loop: true
-})
+  loop: true,
+});
+
+const form = document.querySelector("form");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = form.email.value;
+  const fullname = form.fullname.value;
+  const phone = form.phone.value;
+  const subject = form.subject.value;
+  const message = form.message.value;
+
+  try {
+    const res = await fetch("https://formspree.io/f/xoqznogv", {
+      method: "POST",
+      body: JSON.stringify({ email, fullname, phone, subject, message }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await res.json();
+    if (data) {
+      alert("Message sent");
+      location.assign("/");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
